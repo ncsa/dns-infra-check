464df54499f04800da0691be37752d802ab323dd
Justin Azoff <justin.azoff@gmail.com>
2016-08-05 17:28:09 -0400

    find ns records better.
    This takes the opposite approach of dig, but should still work for our
    purposes OK.
    
    to find name servers for www.foo.com it first checks for NS records for
    www.foo.com, and it that fails it does a query for NS for foo.com.

--- check.go
+++ check.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"strings"
@@ -31,23 +32,47 @@ func check_server(q string, server string) error {
 		return err
 	}
 	for _, r := range response.Answer {
-		log.Printf("    Got response %s", r.(*dns.A).A)
+		log.Printf("    Got response %s", r.String())
 	}
 	return nil
 }
-func check_server_ns(q, server string) error {
-	log.Printf("  Looking up NS record for %s using local server %s", q, server)
 
-	m := new(dns.Msg)
-	m.SetQuestion(q, dns.TypeNS)
-	m.RecursionDesired = true
-	response, _, err := localc.Exchange(m, server)
+func find_ns_records(q, server string) ([]string, error) {
+	var err error
+	var name_servers []string
+	for {
+		log.Printf("  looking up NS records for %s against %s", q, server)
+		m := new(dns.Msg)
+		m.SetQuestion(q, dns.TypeNS)
+		m.RecursionDesired = true
+		response, _, err := localc.Exchange(m, server)
+		if err != nil {
+			log.Printf("  Error looking up %s against %s: %s", q, server, err)
+			return name_servers, err
+		}
+		for _, r := range response.Answer {
+			if ns, ok := r.(*dns.NS); ok {
+				name_servers = append(name_servers, ns.Ns)
+			}
+		}
+		if len(name_servers) > 0 {
+			return name_servers, nil
+		}
+		q = q[strings.Index(q, ".")+1:]
+		if q == "" {
+			return name_servers, errors.New("No servers found")
+		}
+	}
+	return name_servers, err
+}
+
+func check_server_ns(q, server string) error {
+	name_servers, err := find_ns_records(q, server)
 	if err != nil {
-		log.Printf("  Error looking up %s against %s: %s", q, server, err)
+		log.Printf("  Error looking up name servers for %s against %s: %s", q, server, err)
 		return err
 	}
-	for _, r := range response.Answer {
-		ns := r.(*dns.NS).Ns
+	for _, ns := range name_servers {
 		log.Printf("    Got response %s", ns)
 		check_server_ns_resolve(q, server, ns)
 	}
@@ -87,18 +112,15 @@ func check_server_ns_resolve_a(q, ns string) error {
 		return err
 	}
 	for _, r := range response.Answer {
-		rec := r.(*dns.A).A
-		log.Printf("        Got response %s", rec)
+		log.Printf("        Got response %s", r)
 	}
 	successful_queries[q+ns] = true
 	return nil
 }
 
 func check(q string) {
+	q = dns.Fqdn(q)
 	log.Printf("Checking %s", q)
-	if !strings.HasSuffix(q, ".") {
-		q = q + "."
-	}
 	for _, server := range conf.Servers {
 		check_server(q, server+":"+conf.Port)
 		check_server_ns(q, server+":"+conf.Port)
